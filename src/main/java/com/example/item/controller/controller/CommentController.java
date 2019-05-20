52517566dd93ef578a7151d19eb901f2e328bea5
MH <625430265@qq.com>
2019-05-20 08:28:42 +0800

    123

--- src/main/java/com/example/item/controller/controller/CommentController.java
+++ src/main/java/com/example/item/controller/controller/CommentController.java
@@ -28,6 +28,7 @@ public class CommentController {
 		@RequestMapping("/commentView")
 		@Transactional("resTransactionManager")
 		public ModelAndView index(@RequestParam Map<String, Object> param) {
+			System.out.println("1233211234567");
 			UserInfo a = new UserInfo();
 			a.setUserName("毛豪");
 			a.setUserPassword("1233211");
