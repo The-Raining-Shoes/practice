685826a9ba2c7ea3b90e71706d4f9b77167ce7cd
MH <625430265@qq.com>
2019-05-23 22:47:57 +0800

    1

--- src/main/java/com/example/item/controller/controller/PageController.java
+++ src/main/java/com/example/item/controller/controller/PageController.java
@@ -16,7 +16,7 @@ import com.example.item.entity.entityInfo.GoodsInfo;
 @RestController
 @RequestMapping("/page")
 public class PageController {
-	
+
 	@Autowired
 	PageService pageService;
 
@@ -25,47 +25,59 @@ public class PageController {
 	public ModelAndView returnIndex() {
 		return new ModelAndView("/home/index");
 	}
-	
+
 	// 注册页面
 	@RequestMapping("/register")
 	public ModelAndView returnRegist() {
 		return new ModelAndView("/home/register");
 	}
-		
+
 	// 登陆页面
 	@RequestMapping("/login")
 	public ModelAndView registIndex() {
 		return new ModelAndView("/home/login");
-	}	
-	
+	}
+
+	// 我的榴莲
+	@RequestMapping("/memory")
+	public ModelAndView memoryPage() {
+		return new ModelAndView("/home/memory");
+	}
+
+	// 我的榴莲
+	@RequestMapping("/they")
+	public ModelAndView theyPage() {
+		return new ModelAndView("/home/they");
+	}
+
 	// 跳转到他们的榴莲页面
 	@RequestMapping("/test1")
 	public ModelAndView reseachIndex() {
 		return new ModelAndView();
 	}
-	
+
 	// 跳转到我的榴莲
 	@RequestMapping("/test2")
 	public ModelAndView MyIndex() {
 		return new ModelAndView();
 	}
-	
+
 	// 查询所有的榴莲并封装成List返回
 	@RequestMapping("/test3")
 	@ResponseBody
-	public Map<String,List<GoodsInfo>> findAllGoods (){
+	public Map<String, List<GoodsInfo>> findAllGoods() {
 		List<GoodsInfo> goodsInfoList = pageService.findAllGoodsInfo();
-		Map<String,List<GoodsInfo>> result = new HashMap();
+		Map<String, List<GoodsInfo>> result = new HashMap();
 		result.put("result", goodsInfoList);
 		return result;
 	}
-	
+
 	// 保存榴莲
 	@RequestMapping("test4")
 	@ResponseBody
-	public Map<String,String> saveGoods(GoodsInfo goodsInfo) {
+	public Map<String, String> saveGoods(GoodsInfo goodsInfo) {
 		GoodsInfo goodsDb = pageService.saveGoods(goodsInfo);
-		Map<String,String> result = new HashMap();
+		Map<String, String> result = new HashMap();
 		String str = "";
 		if (goodsInfo == null) {
 			str = "保存失败";
@@ -75,5 +87,5 @@ public class PageController {
 		result.put("result", str);
 		return result;
 	}
-	
+
 }
