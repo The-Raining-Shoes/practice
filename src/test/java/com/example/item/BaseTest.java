853cf21418b163c9624fe6f646c8888e704f6b4d
MH <625430265@qq.com>
2019-08-01 16:23:42 +0800

    代码提交

--- src/test/java/com/example/item/BaseTest.java
+++ src/test/java/com/example/item/BaseTest.java
@@ -7,10 +7,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ItemApplicationTests {
+public class BaseTest {
 
 	@Test
-	public void contextLoads() {
+	public void doJob() {
+		
 	}
 
 }
