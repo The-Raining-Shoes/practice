65dcd3b714546c385ab5d48db848006b2631b4e9
maohao <625430265@qq.com>
2020-03-27 14:50:28 +0800

    随便写写

--- src/main/java/com/example/item/TestText.java
+++ src/main/java/com/example/item/TestText.java
@@ -6,21 +6,9 @@ package com.example.item;
  */
 public class TestText {
 
-    static StringBuffer sb = new StringBuffer();
 
     public static void main(String[] args) {
-        Thread thread1 = new Thread(new Threads());
-        Thread thread2 = new Thread(new Threads());
-        thread1.start();
-        thread2.start();
-    }
-}
-
-class Threads implements Runnable {
-
-    @Override
-    public void run() {
-        TestText.sb.append("adc");
-        System.out.println(TestText.sb.toString());
+        String A = "maohao";
+        System.out.println(A.indexOf("h"));
     }
 }
