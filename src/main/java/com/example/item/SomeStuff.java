72a900b3ed68a9ad685fe992a375c3282930de6a
maohao <625430265@qq.com>
2020-07-14 17:27:55 +0800

    随便写写

--- src/main/java/com/example/item/SomeStuff.java
+++ src/main/java/com/example/item/SomeStuff.java
@@ -1,23 +1,28 @@
 package com.example.item;
 
-import org.springframework.stereotype.Component;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 /**
  * 测试数据
  *
  * @author HXM
  * @date 2020年04月13日 9:18
  */
-@Component
 public class SomeStuff {
     public static void main(String[] args) {
-        // 多线程的使用
-        ExecutorService exec = Executors.newFixedThreadPool(300);
-        exec.execute(() -> {
 
-        });
     }
+//    public static void main(String[] args) throws InterruptedException {
+//        // 多线程的使用
+//        ExecutorService exec = Executors.newFixedThreadPool(300);
+//        AtomicInteger i = new AtomicInteger();
+//        exec.execute(() -> {
+//            if (i.get() < 1000000000) {
+//                i.getAndIncrement();
+//                System.out.println(i);
+//            }
+//        });
+//        exec.shutdown();
+//        while (!exec.awaitTermination(1, TimeUnit.MINUTES)) {
+//            System.out.println("其他线程池没有关闭");
+//        }
+//    }
 }
