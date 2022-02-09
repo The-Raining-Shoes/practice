1b910c7c76d873d9a0b012db95eb25a222a905a7
maohao <625430265@qq.com>
2022-02-09 17:01:09 +0800

    随便写写

--- src/test/java/com/example/item/TestRedisTemplate.java
+++ src/test/java/com/example/item/TestRedisTemplate.java
@@ -3,13 +3,14 @@ package com.example.item;
 import com.example.item.domain.entity.TUrlLimiter;
 import com.example.item.domain.jdbc.BeanJdbcTemplate;
 import lombok.Setter;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 public class TestRedisTemplate {
@@ -22,7 +23,19 @@ public class TestRedisTemplate {
 
     @Test
     public void test() {
-        redisTemplate.opsForValue().set("", "", 1, TimeUnit.SECONDS);
+        SimpleClientHttpRequestFactory simpleClientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+        simpleClientHttpRequestFactory.setConnectTimeout(3000);
+        simpleClientHttpRequestFactory.setReadTimeout(100);
+        RestTemplate restTemplate = new RestTemplate(simpleClientHttpRequestFactory);
+        System.out.println(redisTemplate.boundListOps("").size());
+        while (true) {
+            Object o = redisTemplate.boundListOps("").leftPush("1");
+            if (o != null) {
+                System.out.println(o);
+            }else{
+                break;
+            }
+        }
     }
 
     @Test
